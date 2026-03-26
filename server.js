8e86d36182622540fcf842c83d06006122374c81
ZiliHub <hackgpo59@gmail.com>
2026-03-26 22:53:36 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,67 @@
 const express = require("express");
-const axios = require("axios");
 const app = express();
 
-// fake database (sau nâng cấp thành db thật)
 let keys = {
-  "abc123": { hwid: null, expires: Date.now() + 86400000 } // 1 ngày
+    "vip123": {
+        hwid: null,
+        exp: Date.now() + 7 * 86400000,
+        flag: 0
+    }
 };
 
-// API GET SCRIPT
-app.get("/scripts/:id", async (req, res) => {
-  const { key, hwid } = req.query;
+app.get("/l", (req, res) => {
+    res.send(`
+    local k="PUT_KEY_HERE"
+    local h=game:GetService("RbxAnalyticsService"):GetClientId()
+    local r=game:HttpGet("https://YOUR-APP.onrender.com/v?k="..k.."&h="..h)
+    local d=game:GetService("HttpService"):JSONDecode(r)
+    if d.s=="ok" then
+        loadstring(game:HttpGet(d.u))()
+    else
+        warn("Key lỗi: "..d.s)
+    end
+    `)
+})
 
-  if (!keys[key]) return res.send("print('invalid key')");
+app.get("/v", (req, res) => {
+    let { k, h } = req.query
+    let data = keys[k]
 
-  // check hết hạn
-  if (Date.now() > keys[key].expires) {
-    return res.send("print('key expired')");
-  }
+    if (!data) return res.json({ s: "invalid" })
 
-  // lock HWID
-  if (!keys[key].hwid) {
-    keys[key].hwid = hwid;
-  } else if (keys[key].hwid !== hwid) {
-    return res.send("print('hwid not match')");
-  }
+    if (Date.now() > data.exp) {
+        return res.json({ s: "expired" })
+    }
 
-  try {
-    const r = await axios.get("https://raw.githubusercontent.com/USERNAME/REPO/main/main.lua");
-    res.send(r.data);
-  } catch {
-    res.send("print('error loading script')");
-  }
-});
+    if (!data.hwid) {
+        data.hwid = h
+    } else if (data.hwid !== h) {
+        data.flag++
 
-// API TẠO KEY
-app.get("/create-key", (req, res) => {
-  const newKey = Math.random().toString(36).substring(2, 10);
-  keys[newKey] = { hwid: null, expires: Date.now() + 86400000 };
-  res.send(newKey);
-});
+        if (data.flag >= 3) {
+            delete keys[k]
+            return res.json({ s: "banned" })
+        }
 
-app.listen(3000, () => console.log("Server running"));
+        return res.json({ s: "hwid" })
+    }
+
+    res.json({
+        s: "ok",
+        u: "https://raw.githubusercontent.com/ZiliHub/main.lua/refs/heads/main/main.lua?token=GHSAT0AAAAAADXKL762VJN6G4YEPC3ELPQA2OFKWQA"
+    })
+})
+
+app.get("/create", (req, res) => {
+    let key = Math.random().toString(36).substring(2, 10)
+
+    keys[key] = {
+        hwid: null,
+        exp: Date.now() + 7 * 86400000,
+        flag: 0
+    }
+
+    res.json({ key })
+})
+
+app.listen(3000, () => console.log("RUNNING"));
